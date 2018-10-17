364211d958f2ebf357a4955c04552f3f4defa3fc
Z003ZDBC <shiqiang.yu@siemens.com>
2018-10-17 15:20:13 +0800

    10

--- src/main/java/com/test/service/UserServiceImpl.java
+++ src/main/java/com/test/service/UserServiceImpl.java
@@ -1,31 +1,31 @@
-package com.test.service;
-
-
-
-import com.test.model.User;
-import org.hibernate.Query;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
-/**
- * @author Thanos Yu on 2017/3/28.
- */
-@Service
-@Transactional(readOnly = true,rollbackFor = Exception.class)
-public class UserServiceImpl implements UserService {
-
-    @Autowired
-    private SessionFactory sessionFactory;
-
-    @Override
-    @Transactional(readOnly = true,rollbackFor = Exception.class)
-    public List<User> search(){
-        Query query = sessionFactory.getCurrentSession().getNamedQuery("User.all");
-        return query.list();
-    }
-
-}
+//package com.test.service;
+//
+//
+//
+//import com.test.model.User;
+//import org.hibernate.Query;
+//import org.hibernate.SessionFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.List;
+//
+///**
+// * @author Thanos Yu on 2017/3/28.
+// */
+//@Service
+//@Transactional(readOnly = true,rollbackFor = Exception.class)
+//public class UserServiceImpl implements UserService {
+//
+//    @Autowired
+//    private SessionFactory sessionFactory;
+//
+//    @Override
+//    @Transactional(readOnly = true,rollbackFor = Exception.class)
+//    public List<User> search(){
+//        Query query = sessionFactory.getCurrentSession().getNamedQuery("User.all");
+//        return query.list();
+//    }
+//
+//}
